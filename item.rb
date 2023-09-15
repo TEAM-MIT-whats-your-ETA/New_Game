3383a19a912376ff1fd4e78f7d8832a138b6c584
Sako Hotaru <zuoguying@gmail.com>
2023-09-15 10:48:31 +0900

    itemの変更２

---
+++ item.rb
@@ -0,0 +1,17 @@
+class item_blue < Sprite
+  def hit
+    self.vanish
+  end
+end
+
+class item_blue < Sprite
+  def hit
+    self.vanish
+  end
+end
+
+class item_blue < Sprite
+  def hit
+    self.vanish
+  end
+end
